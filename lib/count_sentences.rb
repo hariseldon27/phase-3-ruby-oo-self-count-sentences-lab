999cfc1563e7d40bcf810f9a6adca9318b727988
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-02-25 12:09:04 -0800

    commit tree 9404c0de60d7c84264bf24552751d8064094a9cd

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    ends = []
+    # self.split(" ").each { |el| el.end_with?(".", "?", "!") ? ends << el : nil} #this splits the string at spaces into an array, and if the array element ends with a sentence ending punctuation it gets added to a new array.
+    # return ends.length #then we just return the length of the new array with the sentence enders...
+    # binding.pry
+    self.split(" ").each { |el| el.end_with?(/[.?,!]/) ? ends << el : nil}
+    return ends.length
   end
 end
+binding.pry
